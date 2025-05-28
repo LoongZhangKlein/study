878899eca70ad4714077b34b3d969a85a7270f69
loongzhang <1518736392@qq.com>
2025-05-28 17:07:33 +0800

    amend: 模拟分支冲突Master后第一次提交

--- src/study/test/TestClass.java
+++ src/study/test/TestClass.java
@@ -2,6 +2,7 @@ package study.test;
 
 public class TestClass {
     public static void main(String[] args) {
+        System.out.println("000000000000000000");
         System.out.println("Hello World 主分支===============");
     }
 }
